46e16d77dfeb631c4e90fc7ce3e3afc3b0a3dad1
Cristóvão da Cruz e Silva <CrisXed@gmail.com>
2022-12-15 17:02:34 +0000

    Fixed imports

--- pps_hitmaps/__init__.py
+++ pps_hitmaps/__init__.py
@@ -1,9 +1,9 @@
 __version__ = '0.0.2'
 
-from PPSHitmap import PPSHitmap
-from SensorPad import SensorPad
-from Sensor import Sensor
-from Sensor import calcLossProb
-from CustomizedSensors import *
+from .PPSHitmap import PPSHitmap
+from .SensorPad import SensorPad
+from .Sensor import Sensor
+from .Sensor import calcLossProb
+from .CustomizedSensors import *
 
 __all__ = (["PPSHitmap", "SensorPad", "Sensor", "calcLossProb"] + listOfSensors)
